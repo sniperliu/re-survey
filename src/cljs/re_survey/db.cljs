c549994a82774d57a2ccc038508d5cb7d8104d14
Liu Hao <liuhao@basecity.com>
2017-05-24 17:08:29 +0800

    Add naive model design

--- src/cljs/re_survey/db.cljs
+++ src/cljs/re_survey/db.cljs
@@ -1,4 +1,10 @@
 (ns re-survey.db)
 
 (def default-db
-  {:name "re-frame"})
+  {:name "re-survey"
+   :summary {}
+   :survey [{:title "Which domains are you interested in clojure/clojurescript?"
+             :options ["Web Development"
+                       "Data Science"
+                       "Artificial Intelligence"
+                       "Scientific Calculation"]}]})
